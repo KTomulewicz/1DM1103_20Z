13331d8ec79e32959c26566a42b5d972a72f1e3a
KTomulewicz <jakub.tomulewicz@gmail.com>
2020-11-20 18:57:59 +0100

    Studenci

--- zad5/studenci.c
+++ zad5/studenci.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "studenci.h"
 
-typedef struct _student
-{
-    char imie[25];
-    char nazwisko[50];
-    char nr_albumu[10];
-    char kod_przed[10];
-    char nazwa_przed[255];
-    float ocena;
-    int ects;
-
-} student, *pstudent;
 
 int wczytaj(student dane[100]) 
 
